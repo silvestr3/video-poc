eba0028e1126c15d5b1f03f03efdf15f00aa7c85
silvestr3 <fellipe.silvestre@outlook.com>
2025-03-12 14:41:34 -0300

    feat: :sparkles: Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,42 @@ import "./App.css";
 import { useState } from "react";
 import { Camera } from "lucide-react";
 import ReactPlayer from "react-player";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   const [isCameraOpen, setIsCameraOpen] = useState(false);
   const [videoSource, setVideoSource] = useState<string | null>(null);
 
-  if (isCameraOpen) {
-    return (
-      <CameraView
-        closeCamera={() => setIsCameraOpen(false)}
-        setVideoSource={setVideoSource}
-      />
-    );
-  }
-
   return (
     <>
-      <button
-        style={{
-          padding: "4px 12px",
-          borderRadius: "8px",
-          border: "none",
-          backgroundColor: "#00c899",
-          color: "#fff",
-          cursor: "pointer",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginBottom: "16px",
-        }}
-        onClick={() => setIsCameraOpen(true)}
-      >
-        <Camera />
-      </button>
-      {videoSource && <ReactPlayer controls url={videoSource} />}
+      <Analytics />
+      {isCameraOpen ? (
+        <CameraView
+          closeCamera={() => setIsCameraOpen(false)}
+          setVideoSource={setVideoSource}
+        />
+      ) : (
+        <>
+          <button
+            style={{
+              padding: "4px 12px",
+              borderRadius: "8px",
+              border: "none",
+              backgroundColor: "#00c899",
+              color: "#fff",
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: "16px",
+            }}
+            onClick={() => setIsCameraOpen(true)}
+          >
+            <Camera />
+          </button>
+          {videoSource && <ReactPlayer controls url={videoSource} />}
+        </>
+      )}
     </>
   );
 };
